c5b89a057c32d8c5ed8ba6b5e62124cced4edd6b
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-08-25 22:56:18 +0100

    Enhance error message in FavoritesPage to include a more informative statement when no favorites are found, and ensure text color adapts to dark mode.

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -2,13 +2,16 @@ import { useNavigate } from "react-router-dom";
 import "./NotFound.css";
 import RecipeCard from "../components/RecipeCard";
 import { getRandomColor } from "../libs/utils";
+
 const FavoritesPage = () => {
   const navigate = useNavigate();
   const fav = JSON.parse(localStorage.getItem("favorites")) || [];
   return (
-    <div className=" flex-1 p-10 min-h-screen">
+    <div className="flex-1 p-10 min-h-screen">
       <div className="max-w-screen-lg mx-auto">
-        <p className="font-bold text-3xl md:text-5xl my-4">My Favorites</p>
+        <p className="font-bold text-3xl md:text-5xl my-4 dark:text-white">
+          My Favorites
+        </p>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {fav.map((recipe, index) => (
@@ -19,13 +22,15 @@ const FavoritesPage = () => {
           <section className="home grid h-screen pt-32 pb-16">
             <div className="home__container container grid content-center gap-12 lg:max-w-5xl lg:grid-cols-2 lg:items-center">
               <div className="home__data justify-self-center text-center lg:text-left">
-                <p className="pb-2 font-semibold text-black ">Error 404</p>
-                <h1 className="pb-4 text-5xl font-bold lg:text-6xl text-black  ">
+                <p className="pb-2 font-semibold text-black dark:text-white">
+                  Error 404
+                </p>
+                <h1 className="pb-4 text-5xl font-bold lg:text-6xl text-black dark:text-white">
                   Hey Buddy
                 </h1>
-                <p className="pb-8 font-semibold text-black">
+                <p className="pb-8 font-semibold text-black dark:text-white">
                   We can't seem to find the page <br />
-                  you are looking for.
+                  or what you are looking for.
                 </p>
                 <a
                   href="#"
@@ -46,7 +51,7 @@ const FavoritesPage = () => {
               </div>
             </div>
 
-            <div className="home__footer flex items-center justify-center gap-2 self-end text-sm font-semibold">
+            <div className="home__footer flex items-center justify-center gap-2 self-end text-sm font-semibold dark:text-white">
               <p>mohamedamine.nasri@esprit.tn</p>
             </div>
           </section>
